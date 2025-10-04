3c7ec10d4e20fa9cae1526c85e95322f1e588567
abhinadan meena <meenanandu016@gmail.com>
2025-10-04 13:18:14 +0530

    cors fix error

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,7 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:5173'
-})); // Allow requests from React frontend
+app.use(cors()); // Allow requests from React frontend
 app.use(express.json());
 validateEnv();
 
